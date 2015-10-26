a535c7d5c77bc3b3515c6343313c37616999533b
yaoqj <yaoqj@terminus.io>
2015-10-26 10:05:39 +0800

    add all

---
+++ src/main/java/org/yqj/hive/demo/Main.java
@@ -0,0 +1,13 @@
+package org.yqj.hive.demo;
+
+/**
+ * Created by yaoqijun.
+ * Date:2015-10-26
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class Main {
+    public static void main(String []args){
+        System.out.println("test Hive test");
+    }
+}
---
+++ src/test/org.yqj.hive.demo/TestMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yaoqijun.
+ * Date:2015-10-26
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class TestMain {
+    public static void main(String []args){
+        System.out.println("test Hive");
+    }
+}
